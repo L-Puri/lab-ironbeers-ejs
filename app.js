b45087e1ebd372a2c92ad4354ad5af43f65b37aa
Lokendra Puri <lpuri1979@gmail.com>
2022-05-24 23:01:39 +0100

    punkAPI beer & random beer

--- app.js
+++ app.js
@@ -10,13 +10,15 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(expressLayouts);
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
+app.get('/', (req, res) => {res.render('index');});
+app.get('/beers', (req, res) => {res.render('beers');});
+app.get('/random-beer', (req, res) => {res.render('random-beer');});
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+punkAPI.getBeers()
+.then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+.catch(error => console.log(error));
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
